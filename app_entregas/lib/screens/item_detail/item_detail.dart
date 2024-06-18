ac9cc3947a3a8788ff702ba76b8b09dc318b34e1
PedroGuerraMP <pedrowar.pw@gmail.com>
2024-06-18 01:42:00 -0300

    [~] refactor item_detail_info

--- app_entregas/lib/screens/item_detail/item_detail.dart
+++ app_entregas/lib/screens/item_detail/item_detail.dart
@@ -1,4 +1,5 @@
 import 'package:app_entregas/screens/item_detail/item_detail_info.dart';
+import 'package:app_entregas/screens/item_detail/item_image_stack.dart';
 import 'package:flutter/material.dart';
 import 'package:app_entregas/screens/item_detail/detail_button_row.dart';
 
@@ -9,6 +10,9 @@ class ItemDetail extends StatefulWidget {
   final void Function() redirectToCartDetail;
   
   int numberItems = 0;
+  String titulo = "TituloTitulo";
+  String descricao = "Descrição de prato teste. Foto doida para testar redimensionamento de imagens.";
+  String preco = 'R\$00,00';
 
   @override
   State<StatefulWidget> createState() => _ItemDetailState();
@@ -27,34 +31,39 @@ class _ItemDetailState extends State<ItemDetail>{
             minWidth: MediaQuery.of(context).size.width,
             minHeight: MediaQuery.of(context).size.height,
           ),
-          child: Column(
+          child: SafeArea(
+            child: Column(
               mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Column(
-                  children: [
-                    DetailItemInfo(widget.redirectToScaffoldMenu),
-                    const Divider(),
-                    const Align(
-                      alignment: Alignment.topLeft,
-                      child: Text("Observações",
-                        style: TextStyle(fontSize: 18, color: Colors.black87),
-                      ),
-                    ),
-                    const Card(
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          maxLines: 4, //or null 
-                          decoration: InputDecoration.collapsed(hintText: "Peça para caprichar ou retirar algum ingrediente"),
-                        ),
-                      )
-                    ),
-                  ],
+              children: [
+                ItemImageStack(widget.redirectToScaffoldMenu, 'assets/img/prato-teste.jpg'),
+                const Divider( height: 0, ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width*0.9,
+                  child: DetailItemInfo(widget.titulo, widget.descricao, widget.preco),
+                ),                
+                const Divider(),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text("Observações",
+                    style: TextStyle(fontSize: 18, color: Colors.black87),
+                  ),
                 ),
-              ],
+                const Card(
+                  elevation: 1,
+                  shape: BeveledRectangleBorder(),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      maxLines: 4, //or null 
+                      decoration: InputDecoration.collapsed(hintText: "Peça para caprichar ou retirar algum ingrediente"),
+                    ),
+                  )
+                ),
+              ]
             ),
           ),
         ),
+      )
     );
   }
 }
--- app_entregas/lib/screens/item_detail/item_detail_info.dart
+++ app_entregas/lib/screens/item_detail/item_detail_info.dart
@@ -1,65 +1,41 @@
 import 'package:flutter/material.dart';
 
 class DetailItemInfo extends StatelessWidget {
-  const DetailItemInfo(this.scaffoldMenuOnClick, {super.key});
+  DetailItemInfo(this.titulo, this.descricao, this.preco, {super.key});
 
-  final void Function() scaffoldMenuOnClick;
+  String titulo, descricao, preco = "";
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            const SizedBox(height: 20,),
-            SizedBox(
-              height: 250,
-              child: Stack(
-                children: [
-                  SizedBox(
-                      height: 250,
-                      width: MediaQuery.of(context).size.width,
-                      child: Image.asset(
-                        'assets/img/prato-teste.jpg',
-                      )),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: 250,
-                      child: ColoredBox(color: Colors.black.withOpacity(0.5))),
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    style: const ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Colors.white)),
-                    onPressed: () { scaffoldMenuOnClick(); },
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-        const Divider(
-          height: 0,
-        ),
-        const Align(
+        Align(
           alignment: Alignment.topLeft,
           child: Text(
-            "Titulo",
-            style: TextStyle(fontSize: 30),
+            style: const TextStyle(fontSize: 20),
+            titulo,
           ),
         ),
-        const SizedBox(
-          height: 20,
+        const SizedBox(height: 10,),
+        SizedBox(
+          width: MediaQuery.of(context).size.width*0.8,
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              style: const TextStyle(fontSize: 16, color: Colors.black87),
+              overflow: TextOverflow.fade,
+              maxLines: 2,
+              descricao,
+            ),
+          ),
         ),
-        const Align(
-          alignment: Alignment.topLeft,
+        const SizedBox(height: 10,),
+        Align(
+          alignment: Alignment.bottomRight,
           child: Text(
-            "Descrição de prato teste. Foto doida para testar redimensionamento de imagens.",
-            style: TextStyle(fontSize: 20, color: Colors.black87),
+            style: const TextStyle(fontSize: 16, color: Colors.black87),
+            preco,
           ),
-        ),
-        const SizedBox(
-          height: 40,
         ),
       ],
     );
