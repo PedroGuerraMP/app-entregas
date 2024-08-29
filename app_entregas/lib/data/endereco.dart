6e326518e13eb4955d69abf8efcb5822588b2d88
PedroGuerraMP <pedrowar.pw@gmail.com>
2024-08-29 10:19:52 -0300

    switch logic

---
+++ app_entregas/lib/data/endereco.dart
@@ -0,0 +1,6 @@
+import 'package:app_entregas/models/endereco.dart';
+
+final enderecos = [
+  Endereco(0, "31505-000", "Rua Dom Pedro II, 125", "Centro", "Sabará"),
+  Endereco(1, "31505-000", "Rua Dom Pedro II, 125", "Centro", "Sabará"),
+];
---
+++ app_entregas/lib/models/endereco.dart
@@ -0,0 +1,9 @@
+class Endereco {
+  int id;
+  String cep;
+  String logradouro;
+  String bairro;
+  String cidade;
+
+  Endereco(this.id, this.cep , this.logradouro, this.bairro, this.cidade);
+}
